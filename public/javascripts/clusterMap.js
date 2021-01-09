3241c6dadc9d9d2ba2e7a4c31b756028eb0f63e9
kanchi2438 <jainkanchi2001@gmail.com>
2021-01-10 03:28:31 +0530

    changed map center

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -2,7 +2,7 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
 container: 'map',
 style: 'mapbox://styles/mapbox/outdoors-v11',
-center: [-103.59179687498357, 40.66995747013945],
+center: [77.40862371384621, 23.535333924008786],
 zoom: 3
 });
  
